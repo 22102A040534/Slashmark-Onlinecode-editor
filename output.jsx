bab5699b018dad6155e09b4e8b6025e8efe3e778
ARSHIA NOORAIEN <142517895+22102A040534@users.noreply.github.com>
2024-07-27 16:46:28 +0530

    Create output.jsx

---
+++ output.jsx
@@ -0,0 +1,13 @@
+// src/components/Output.js
+import React from 'react';
+
+const Output = ({ output }) => {
+  return (
+    <div>
+      <h3>Output</h3>
+      <pre>{output}</pre>
+    </div>
+  );
+};
+
+export default Output;
